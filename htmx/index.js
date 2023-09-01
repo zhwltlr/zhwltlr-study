bd4a9db757aa788f803d95d03b69ec0f889d6b93
zhwltlr <vyoungv7340@naver.com>
2023-09-01 23:47:16 +0900

    QNA board example with htmx-express

--- htmx/index.js
+++ htmx/index.js
@@ -2,15 +2,56 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+let posts = [];
+
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
 });
 
-app.get("/data", (req, res) => {
-  const data = { message: "Hello from the server!" };
-  res.json(data);
+app.get("/api/posts", (req, res) => {
+  res.json(posts);
+});
+
+app.post("/api/posts", (req, res) => {
+  const newPost = {
+    id: Date.now(),
+    title: req.body.title,
+    content: req.body.content,
+  };
+  posts.push(newPost);
+  res.status(201).json(newPost);
+});
+
+app.put("/api/posts/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  const updatedPost = {
+    id: id,
+    title: req.body.title,
+    content: req.body.content,
+  };
+  const index = posts.findIndex((post) => post.id === id);
+  if (index !== -1) {
+    posts[index] = updatedPost;
+    res.json(updatedPost);
+  } else {
+    res.status(404).json({ error: "Post not found" });
+  }
+});
+
+app.delete("/api/posts/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  const index = posts.findIndex((post) => post.id === id);
+  if (index !== -1) {
+    posts.splice(index, 1);
+    res.sendStatus(204);
+  } else {
+    res.status(404).json({ error: "Post not found" });
+  }
 });
 
 app.listen(port, () => {
-  console.log(`Server is running at http://localhost:${port}`);
+  console.log(`Server is running on port ${port}`);
 });

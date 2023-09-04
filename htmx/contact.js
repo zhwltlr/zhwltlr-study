dfb2ccc875cd3128345e1698f0c283a152bcde81
zhwltlr <vyoungv7340@naver.com>
2023-09-04 17:36:38 +0900

    contact list with htmx (CRUD)

---
+++ htmx/contact.js
@@ -0,0 +1,22 @@
+module.exports = [
+  {
+    id: 0,
+    name: "ddJoe Smith",
+    email: "joe@smith.org",
+  },
+  {
+    id: 1,
+    name: "Angie MacDowell",
+    email: "angie@macdowell.org",
+  },
+  {
+    id: 2,
+    name: "Fuqua Tarkenton",
+    email: "fuqua@tarkenton.org",
+  },
+  {
+    id: 3,
+    name: "Kim Yee",
+    email: "kim@yee.org",
+  },
+];
--- htmx/index.js
+++ htmx/index.js
@@ -6,13 +6,13 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 let posts = require("./posts");
+let contact = require("./contact");
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
 });
 
 function generatePostListHTML(posts) {
-  // <small>작성 시간: ${post.time}</small>
   let html = "";
   posts.forEach((post) => {
     html += `<li class="list-group-item">
@@ -60,6 +60,151 @@ app.delete("/api/posts/:id", (req, res) => {
   if (index !== -1) {
     posts.splice(index, 1);
     res.sendStatus(204);
+  } else {
+    res.status(404).json({ error: "Post not found" });
+  }
+});
+
+// test
+function contactListHTML(conatact) {
+  let html = "";
+  conatact.forEach((cont) => {
+    html += `<tr id="contact-list">
+        <td>${cont.name}</td>
+        <td>${cont.email}</td>
+        <td>
+          <button class="btn btn-danger"
+                  hx-get="/contact/${cont.id}/edit"
+                  hx-target="closest tr"
+                  hx-swap="outerHTML"
+                  hx-trigger="edit"
+                  data-contact-id="${cont.id}"
+                  _="on click
+                      if .editing is not empty
+                        Swal.fire({title: 'Already Editing',
+                                    showCancelButton: true,
+                                    confirmButtonText: 'Yep, Edit This Row!',
+                                    text:'Hey!  You are already editing a row!  Do you want to cancel that edit and continue?'})
+                        if the result's isConfirmed is false
+                          halt
+                        end
+                        send cancel to .editing
+                      end
+                      trigger edit">
+            Edit
+          </button>
+          <button class="btn btn-danger" hx-delete="/contact/${cont.id}" hx-confirm="Are you sure?" hx-target="#contactList" hx-swap="innerHTML" hx-include="closest tr">
+          Delete
+        </button>
+        </td>
+      </tr>`;
+  });
+  return html;
+}
+
+app.get("/contact", (req, res) => {
+  const contactList = contactListHTML(contact);
+  res.send(contactList);
+});
+
+app.get("/contact/:id/edit", (req, res) => {
+  const id = parseInt(req.params.id);
+  const index = contact.findIndex((cont) => cont.id === id);
+  if (index !== -1) {
+    const contactToEdit = contact[index];
+    const editForm = `
+          <tr class='editing' data-contact-id="${contactToEdit.id}">
+              <td><input name='name' value='${contactToEdit.name}'></td>
+              <td><input name='email' value='${contactToEdit.email}'></td>
+              <td>
+                  <button class="btn btn-danger" hx-get="/contact" hx-target="#contactList">
+                      Cancel
+                  </button>
+                  <button class="btn btn-danger" hx-put="/contact/${contactToEdit.id}" hx-target="#contactList" hx-swap="innerHTML" hx-include="closest tr">
+                      Save
+                  </button>
+              </td>
+          </tr>
+      `;
+    res.send(editForm);
+  } else {
+    res.status(404).json({ error: "Contact not found" });
+  }
+});
+
+app.post("/contact", (req, res) => {
+  const newContact = {
+    id: contact.length + 1,
+    name: req.body.name,
+    email: req.body.email,
+  };
+  contact.push(newContact);
+  const newContactList = contactListHTML(contact);
+  res.send(newContactList);
+  // res.status(201).json(newPost);
+});
+
+app.put("/contact/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  const updatedContact = {
+    id: id,
+    name: req.body.name,
+    email: req.body.email,
+  };
+  const index = contact.findIndex((cont) => cont.id === id);
+  if (index !== -1) {
+    contact[index] = updatedContact;
+    const updateContactList = contactListHTML(contact);
+    res.send(updateContactList);
+  } else {
+    res.status(404).json({ error: "Post not found" });
+  }
+});
+
+app.get("/contact/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  const index = contact.findIndex((cont) => cont.id === id);
+  if (index !== -1) {
+    const cancelContact = `
+        <tr id="contact-list">
+        <td>${contact[index].name}</td>
+        <td>${contact[index].email}</td>
+        <td>
+          <button class="btn btn-danger"
+                  hx-get="/contact/${contact[index].id}/edit"
+                  hx-target="#contact-list"
+                  hx-swap="outerHTML"
+                  hx-trigger="edit"
+                  _="on click
+                      if .editing is not empty
+                        Swal.fire({title: 'Already Editing',
+                                    showCancelButton: true,
+                                    confirmButtonText: 'Yep, Edit This Row!',
+                                    text:'Hey!  You are already editing a row!  Do you want to cancel that edit and continue?'})
+                        if the result's isConfirmed is false
+                          halt
+                        end
+                        send cancel to .editing
+                      end
+                      trigger edit">
+            Edit
+          </button>
+        </td>
+      </tr>
+    `;
+    res.send(cancelContact);
+  } else {
+    res.status(404).json({ error: "Post not found" });
+  }
+});
+
+app.delete("/contact/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  const index = contact.findIndex((cont) => cont.id === id);
+  if (index !== -1) {
+    contact.splice(index, 1);
+    const deleteContactList = contactListHTML(contact);
+    res.send(deleteContactList);
   } else {
     res.status(404).json({ error: "Post not found" });
   }

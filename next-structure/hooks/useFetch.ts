a73d29b1c398632f2b92c0585c514414beeafd32
zhwltlr <vyoungv7340@naver.com>
2024-03-04 21:53:30 +0900

    add useFetch hook

---
+++ next-structure/hooks/useFetch.ts
@@ -0,0 +1,27 @@
+import { useState, useEffect } from "react";
+
+const useFetch = (url: string) => {
+  const [data, setData] = useState<any>(null);
+  const [loading, setLoading] = useState<boolean>(true);
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(url);
+        const jsonData = await response.json();
+        setData(jsonData);
+      } catch (error) {
+        setError(error.message);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, [url]);
+
+  return { data, loading, error };
+};
+
+export default useFetch;
